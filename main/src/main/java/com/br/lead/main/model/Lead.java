dea8162437fca20f45603bb3127b92dcf92098c5
andre-bandeli <andre.lbandeli@gmail.com>
2022-11-09 02:13:36 -0300

    feat: classe model

---
+++ main/src/main/java/com/br/lead/main/model/Lead.java
@@ -0,0 +1,79 @@
+package com.br.lead.main.model;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Lead {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @NotNull
+    private String nome;
+
+    @NotNull
+    private Integer ddd;
+
+    @NotNull
+    private Integer telefone;
+
+    @NotNull
+    private String email;
+
+    public Lead() {
+    }
+
+    public Lead(Long id, String nome, Integer ddd, Integer telefone, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.ddd = ddd;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(Integer ddd) {
+        this.ddd = ddd;
+    }
+
+    public Integer getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Integer telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
