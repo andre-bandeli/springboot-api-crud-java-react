141b6fafba483d967cf2990989e9fd6d1ba5c0cc
andre-bandeli <andre.lbandeli@gmail.com>
2022-12-08 20:00:39 -0300

    usuario model

---
+++ user/src/main/java/com/user/user/model/Usuario.java
@@ -0,0 +1,73 @@
+package com.user.user.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String nome;
+
+    private String email;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String username, String password, String nome, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
