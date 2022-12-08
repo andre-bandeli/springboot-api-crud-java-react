99f0a6695f6c5a43f1f6d901ca440961edadef74
andre-bandeli <andre.lbandeli@gmail.com>
2022-12-08 20:10:19 -0300

    usuario service

---
+++ user/src/main/java/com/user/user/service/UsuarioService.java
@@ -0,0 +1,48 @@
+package com.user.user.service;
+
+import com.user.user.model.Usuario;
+import com.user.user.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario adicionarFuncionario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public Optional<Usuario> getUsuarioById(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public List<Usuario> listUsuarios() {
+        return (List<Usuario>) usuarioRepository.findAll();
+    }
+
+    public List<Usuario> adicionarListaUsuarios(List<Usuario> usuarioList) {
+        return (List<Usuario>) usuarioRepository.saveAll(usuarioList);
+    }
+
+    public void deletaUsuario(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+
+    public void updateUsuario(Long id) {
+        usuarioRepository.findById(id).map(
+                usuario -> {
+                    usuario.setNome(usuario.getNome());
+                    usuario.setEmail(usuario.getEmail());
+                    usuario.setUsername(usuario.getUsername());
+                    usuario.setPassword(usuario.getPassword());
+                    return "atualizado!!";
+                }
+        );
+    }
+}
